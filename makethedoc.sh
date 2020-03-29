477b232ead3808cd7cdb4fa468cc069b421d84b4
Jan Heiland <jnhlnd@gmail.com>
2020-03-29 12:25:54 +0200

    upds

--- makethedoc.sh
+++ makethedoc.sh
@@ -3,10 +3,10 @@ MDFILE=slides.md
 pandoc $MDFILE -o index.html \
     --filter pandoc-citeproc -t revealjs --slide-level=2 -s \
     --mathjax='node_modules/mathjax/es5/tex-mml-chtml.js' \
-    -V revealjs-url=node_modules/reveal.js \
+    -V revealjs-url=https://revealjs.com \
     -V theme=solarized -V hash=true \
     -V viewDistance=15 -V width=1280 -V height=960 -V margin=0.05
-    # -V revealjs-url=https://revealjs.com \
+    # -V revealjs-url=node_modules/reveal.js \
     # --mathjax='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML' \
 
 # ## base theme slides -- works well
